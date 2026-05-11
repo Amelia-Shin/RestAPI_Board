f491e8eb783a866f6379eb8f041eccfa2a9ad461
Amelia-Shin <gmldnjs0402@naver.com>
2026-05-12 07:35:27 +0900

    feat: 게시글 작성 기능 추가

--- react_p/src/pages/CreatePost.js
+++ react_p/src/pages/CreatePost.js
@@ -1,20 +1,55 @@
-import React from "react";
-import "./../css/CreatePost.css";
+import "../css/CreatePost.css";
+import { useState } from "react";
+import axios from "axios";
 
 function CreatePost() {
+  let [formData, setFormData] = useState({
+    title: "",
+    content: "",
+  });
+
+  let onChangeFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onClickSubmit = () => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/posts`, formData)
+      .then((response) => {
+        console.log("게시글 등록 성공:", response.data);
+      })
+      .catch((error) => {
+        console.error("게시글 등록 실패:", error);
+      });
+  };
+
   return (
     <div className="create-post-container">
       <h1 className="create-post-title">게시글 작성</h1>
       <form className="create-post-form">
         <div className="form-group">
           <label htmlFor="title">제목</label>
-          <input id="title" type="text" placeholder="제목을 입력하세요" />
+          <input
+            id="title"
+            name="title"
+            type="text"
+            placeholder="제목을 입력하세요"
+            onChange={onChangeFormData}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="content">내용</label>
-          <textarea id="content" placeholder="내용을 입력하세요" />
+          <textarea
+            id="content"
+            name="content"
+            placeholder="내용을 입력하세요"
+            onChange={onChangeFormData}
+          />
         </div>
-        <button type="submit" className="submit-button">
+        <button type="button" className="submit-button" onClick={onClickSubmit}>
           등록
         </button>
       </form>
